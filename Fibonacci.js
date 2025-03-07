0a2a87767d9037792f08e9da757493e1ce30369f
Cody Fisher <fishercody23@gmail.com>
2025-03-06 21:18:41 -0700

    Fixed the fibonacci functions, misunderstood the question.

--- Fibonacci.js
+++ Fibonacci.js
@@ -1,33 +1,31 @@
 // Regular fibonacci
-function fibonacci() {
+function fibs(total) {
     const array = []
     let a = 0
     let b = 1
-    for(let i = 0; i < 10; i++) {
+    let count = 0
+    for(let i = 0; count < total; i++) {
         
         array.push(a)
         let next = a + b
 
         a = b
         b = next
-        
+        count++
     } 
     console.log(array);
 }
-fibonacci();
+fibs(8);
 
 // Solved with recurrision 
-function fibonacciRec(n, start = 0) {
-    let a = start
-    let b = n
+function fibonacciRec(total, a = 0, b = 1, array = []) {
+   if(total === 0) {
+    return array
+   } 
 
-    if(b > 21) {
-        return n
-    } else {
-        console.log(a)
-        let next = a + b
-        fibonacciRec(next, b)
-    }
+   array.push(a)
+
+   return fibonacciRec(total - 1, b, a + b, array)
 }
 
-fibonacciRec(1)
+console.log(fibonacciRec(8))
